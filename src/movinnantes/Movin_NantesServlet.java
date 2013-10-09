5ea8f073035f971d377ce2ced7c2118c80d35b92
Coraline MARIE <E137286Z@I001V2pc09.ensinfo.sciences.univ-nantes.prive>
2013-10-09 15:15:58 +0200

    Commit initial, création du projet Eclipse

---
+++ src/movinnantes/Movin_NantesServlet.java
@@ -0,0 +1,13 @@
+package movinnantes;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Movin_NantesServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Coming soon");
+	}
+}
